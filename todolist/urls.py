4aa732481e1a6da989fe733f83b1a15a3bae2fb3
auliafikrialkhawariz <afikria15@gmail.com>
2022-10-13 11:18:07 +0700

    Tugas 6

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,6 +1,6 @@
 import imp
 from django.urls import path
-from todolist.views import delete, show_todolist, register, login_user, logout_user, create_task, delete, done
+from todolist.views import delete_ajax, show_todolist, register, login_user, logout_user, create_task, delete, done, show_json, submit_ajax, show_todolist_ajax, delete_ajax, done_ajax
 
 app_name = 'todolist'
 
@@ -12,4 +12,9 @@ urlpatterns = [
     path('create-task/', create_task, name='create-task'),
     path('delete/<int:id>/', delete, name='delete'),
     path('done/<int:id>/', done, name='done'),
+    path('json/', show_json, name="show_json"),
+    path('/submit', submit_ajax, name="submit_ajax"),
+    path('ajax/', show_todolist_ajax, name="show_todolist_ajax"),
+    path('delete_ajax/<int:id>/', delete_ajax, name='delete_ajax'),
+    path('done_ajax/<int:id>/', done_ajax, name='done_ajax'),
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,15 +1,17 @@
 import datetime
 from django.shortcuts import render
 from django.urls import reverse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponseNotFound, HttpResponse
+from django.core import serializers
 from django.shortcuts import redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from django.contrib.auth import authenticate, login
-from django.contrib.auth import logout
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-from todolist.form import TaskForm
+from django.views.decorators.csrf import csrf_exempt
 from todolist.models import Task
+from todolist.form import TaskForm
 
 # Create your views here.
 
@@ -23,6 +25,22 @@ def show_todolist(request):
         'last_login': request.COOKIES['last_login'],
     }
     return render(request, 'todolist.html', context)
+
+@login_required(login_url='/todolist/login/')
+def show_todolist_ajax(request):
+    data_todolist = Task.objects.all()
+    context = {
+        'list_data': data_todolist,
+        'nama': 'Aulia Fikri Al Khawariz',
+        'npm': '2106701160',
+        'last_login': request.COOKIES['last_login'],
+    }
+    return render(request, 'todolist_ajax.html', context)
+
+@login_required(login_url='/todolist/login/')
+def show_json(request):
+    data = Task.objects.filter(user = request.user)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 def register(request):
     form = UserCreationForm()
@@ -73,10 +91,25 @@ def create_task(request):
     context = {'form':form}
     return render(request, 'create-task.html', context)
 
+def submit_ajax(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        
+        new_data = Task(user=request.user, title=title, description=description, date=datetime.datetime.now())
+        new_data.save()
+        return HttpResponse(b"CREATED", status=201)
+    return HttpResponseNotFound()
+
 def delete(request, id):
     data = Task.objects.get(id=id)
     data.delete()
     return redirect('todolist:show_todolist')
+
+def delete_ajax(request, id):
+    data = Task.objects.get(id=id)
+    data.delete()
+    return redirect('todolist:show_todolist_ajax')
 
 def done(request, id):
     data = Task.objects.get(id=id)
@@ -86,3 +119,12 @@ def done(request, id):
         data.done = False
     data.save()
     return redirect('todolist:show_todolist')
+
+def done_ajax(request, id):
+    data = Task.objects.get(id=id)
+    if data.done == False:
+        data.done = True
+    else:
+        data.done = False
+    data.save()
+    return redirect('todolist:show_todolist_ajax')
